81f0e902aea6bc196cc1b16183cbad5d24e94f75
sstanislaur <lisnychukstas@gmail.com>
2023-04-11 16:08:39 -0700

    init

---
+++ src/components/SimsponComponent.js
@@ -0,0 +1,18 @@
+
+const SimsponComponent = ({data: simpsons}) => {
+    return (
+        <div>
+            {
+                simpsons.map(value =>
+                    <p>
+                        <div>Name: {value.name} {value.surname}</div>
+                        <div>Age: {value.age}</div>
+                        <div>Info: {value.info}</div>
+                        <div><img src={value.photo} alt=""/></div>
+                    </p>)
+            }
+        </div>
+    );
+};
+
+export default SimsponComponent;
